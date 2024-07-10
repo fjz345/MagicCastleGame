e500d69cc8d495448fcb35a61a87c8185770a5e0
Filip Zachrisson <fjz345@hotmail.com>
2024-07-10 19:56:19 +0200

    Initial commit

---
+++ Source/MagicCastleGame/MagicCastleGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MagicCastleGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MagicCastleGame, "MagicCastleGame" );
